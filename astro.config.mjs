fd5aab0cdbc4a2a7db95423a87ef6dae49c6f051
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-07-27 20:19:35 +0200

    fix #4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
     "/": "/en/",
   },
+
+  adapter: netlify(),
 });
